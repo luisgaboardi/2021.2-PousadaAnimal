27513a8b75d3d570f937e971aa39cb710961ed52
luisgaboardi <luis.ggaboardi@gmail.com>
2022-03-02 16:07:34 -0300

    Install dependencies

--- DjangoBackend/DjangoBackend/settings.py
+++ DjangoBackend/DjangoBackend/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # Our apps
+    'rest_framework',  # For the serializers and endpoints
+    'knox',  # For user authentication
 ]
 
 MIDDLEWARE = [
@@ -121,3 +125,11 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        # 'rest_framework.authentication.BasicAuthentication',
+        # 'rest_framework.authentication.SessionAuthentication',
+        'knox.auth.TokenAuthentication',
+    ]
+}
