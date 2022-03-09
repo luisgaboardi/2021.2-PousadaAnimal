5a9b923ff1fb2ff4963ea53df3abe967937b1908
luisgaboardi <luis.ggaboardi@gmail.com>
2022-03-09 18:04:59 -0300

    User registration only if authenticated and data visibility reduced

--- djangoBackend/djangoBackend/urls.py
+++ djangoBackend/djangoBackend/urls.py
@@ -16,13 +16,10 @@ Including another URLconf
 
 from django.urls import include, path
 from django.contrib import admin
-from rest_framework import routers
-from users.views import UserViewSet
 
-router = routers.DefaultRouter()
-router.register(r'users', UserViewSet)
 
 urlpatterns = [
-    path('', include(router.urls)),
+    path('', include('users.urls')),
+    path('api-auth/', include('rest_framework.urls')),
     path('admin/', admin.site.urls),
 ]
--- djangoBackend/users/views.py
+++ djangoBackend/users/views.py
@@ -1,13 +1,31 @@
-from .models import User
-from rest_framework import viewsets
+from users.models import User
+from users.serializers import UserSerializer
+from rest_framework import generics
+from rest_framework.views import APIView
 from rest_framework import permissions
-from .serializers import UserSerializer
+from rest_framework.response import Response
+from rest_framework import status
 
 
-class UserViewSet(viewsets.ModelViewSet):
+class UserList(APIView):
     """
-    API endpoint that allows users to be viewed or edited.
+    List all users, or create a new user.
     """
+    def get(self, request, format=None):
+        if request.user.is_staff:
+            users = User.objects.all()
+            serializer = UserSerializer(users, many=True)
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(status=status.HTTP_401_UNAUTHORIZED)
+
+    def post(self, request, format=None):
+        serializer = UserSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class UserDetail(generics.RetrieveAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
-    # permission_classes = [permissions.IsAuthenticated]
+    permission_classes = [permissions.IsAuthenticated]
