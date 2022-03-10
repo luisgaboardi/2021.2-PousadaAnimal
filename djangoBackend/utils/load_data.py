a6250878cead15f41c1c53d5071ce575191bc570
luisgaboardi <luis.ggaboardi@gmail.com>
2022-03-10 14:02:22 -0300

    Add scripts to populate database

---
+++ djangoBackend/utils/load_data.py
@@ -0,0 +1,18 @@
+from os import system
+
+# Database
+system('python3 manage.py makemigrations users')
+system('python3 manage.py migrate')
+
+# Create example users
+print('Essa operação vai sobrescrever os dados salvos no banco de dados.')
+print('Você deseja carregar os dados? (Y/n)')
+
+user_input = input()
+
+if user_input.lower() == 'y':
+    system('python manage.py loaddata users')
+
+# Super User
+print('Create super user')
+system('python3 manage.py createsuperuser')
