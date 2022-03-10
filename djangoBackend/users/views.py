afdf277a65605577de821f746da73d2f31685c75
luisgaboardi <luis.ggaboardi@gmail.com>
2022-03-10 14:31:37 -0300

    Update request response status

--- djangoBackend/users/views.py
+++ djangoBackend/users/views.py
@@ -6,7 +6,6 @@ from rest_framework import generics
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
-from rest_framework.authtoken.models import Token
 
 
 class UserList(APIView):
@@ -33,7 +32,7 @@ class UserList(APIView):
 
 class UserDetail(generics.RetrieveAPIView):
     """
-    Ger one user detail by pk.
+    Get one user detail by pk.
     """
     def get_object(self, pk):
         try:
@@ -45,5 +44,5 @@ class UserDetail(generics.RetrieveAPIView):
         if request.user in User.objects.all():
             user = self.get_object(pk)
             serializer = UserSerializer(user)
-            return Response(serializer.data)
+            return Response(serializer.data, status=status.HTTP_200_OK)
         return Response(status=status.HTTP_403_FORBIDDEN)
