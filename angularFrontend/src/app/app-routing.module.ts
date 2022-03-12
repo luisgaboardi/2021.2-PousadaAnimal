487e6433b5a9c10a8ae3699dc54e0a9fb279826f
Lais Portela <laisportela@wizsolucoes.com.br>
2022-03-12 00:56:16 -0300

    Adicionando rotas para o login,home e register

--- angularFrontend/src/app/app-routing.module.ts
+++ angularFrontend/src/app/app-routing.module.ts
@@ -1,10 +1,32 @@
+
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
-
+const routes: Routes = [
+  {path: '', redirectTo: 'home', pathMatch:'full'},
+  // {
+  //   path: '',
+  //   loadChildren: () =>
+  //     import('./feature/panel/panel.module').then((x) => x.PanelModule)
+  // },
+  {
+    path: 'auth',
+      loadChildren: () =>
+        import('./feature/auth/auth.module').then((x) => x.AuthModule)
+  },
+  {
+    path: 'home',
+      loadChildren: () =>
+        import('./feature/home/home.module').then((x)=> x.HomeModule)
+  },
+  // {
+  //   path: '**',
+  //   redirectTo: '',
+  // },
+];
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes), FormsModule, ReactiveFormsModule],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- angularFrontend/src/app/app.module.ts
+++ angularFrontend/src/app/app.module.ts
@@ -1,3 +1,6 @@
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule} from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { RouterModule } from '@angular/router';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
@@ -5,20 +8,13 @@ import { IConfig, NgxMaskModule } from 'ngx-mask';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { RegisterComponent } from './feature/register/register.component';
 import { NavBarComponent } from './feature/nav-bar/nav-bar.component';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { HttpClientModule} from '@angular/common/http';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 
 export let options: Partial<IConfig> | (() => Partial<IConfig>);
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    RegisterComponent,
-    NavBarComponent
-  ],
+  declarations: [AppComponent,NavBarComponent],
   imports: [
     ReactiveFormsModule,
     FormsModule,
