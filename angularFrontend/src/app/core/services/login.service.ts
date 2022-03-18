ef18d05eba72e48af071a67b635292b1a65a38f3
luisgaboardi <luis.ggaboardi@gmail.com>
2022-03-18 16:49:25 -0300

    Add persistent login via token
    Co-authored-by: Ana Julia <aluzianobriceno@gmail.com>

--- angularFrontend/src/app/core/services/login.service.ts
+++ angularFrontend/src/app/core/services/login.service.ts
@@ -2,21 +2,40 @@ import { environment } from './../../../environments/environment';
 import { LoginClient } from './../../../shared/models/login-client';
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { BehaviorSubject, Observable } from 'rxjs';
+import { User } from 'src/app/models/user.model';
+import { Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
 })
 export class LoginService {
 
-  token: String;
+  private userSubject: BehaviorSubject<User>;
+  public user: Observable<User>;
 
-  constructor(private readonly http: HttpClient) { }
-
-  sendLoginClient( newUser: LoginClient) {
-    this.http.post<any>(`${environment.endPointPousadaAnimal}/users/login/`, newUser).subscribe(data => {
-      this.token = data
-    })
-    return this.token
+  constructor(private http: HttpClient, private router: Router) {
+    this.userSubject = new BehaviorSubject<User>(
+      JSON.parse(localStorage.getItem('currentUser'))
+    );
+    this.user = this.userSubject.asObservable();
   }
+
+  sendLoginClient( requestData: LoginClient) {
+    return this.http.post<any>(`${environment.endPointPousadaAnimal}/users/login/`, requestData).subscribe(token => {
+      let user: User = {
+        email: requestData.email,
+        token: token,
+      };
+      localStorage.setItem('currentUser', JSON.stringify(user));
+      this.userSubject.next(user);
+      return user;
+    })
+  }
+
+  logout() {
+    localStorage.removeItem('currentUser');
+    this.userSubject.next(null);
+  }
+  
 }
