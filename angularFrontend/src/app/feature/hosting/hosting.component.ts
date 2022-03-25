2a7752a655908672d9537ddc4f8d1545d0402de4
luisgaboardi <luis.ggaboardi@gmail.com>
2022-03-25 15:28:02 -0300

    Make hosting page and fix hosting model

--- angularFrontend/src/app/feature/hosting/hosting.component.ts
+++ angularFrontend/src/app/feature/hosting/hosting.component.ts
@@ -1,6 +1,6 @@
 import { HostingService } from './../../core/services/hosting.service';
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 
 
@@ -11,24 +11,30 @@ import { Router } from '@angular/router';
 })
 export class HostingComponent implements OnInit {
 
-  formHosting: FormGroup = new FormGroup ({
-    pet: new FormControl('',[Validators.required]),
-    start_date: new FormControl('',[Validators.required]),
-    end_date: new FormControl('',[Validators.required]),
-    cost: new FormControl('',[Validators.required]),
-    observation: new FormControl('',[Validators.required]),
-    approved: new FormControl('',[Validators.required]),
+  formHosting: FormGroup = new FormGroup({
+    owner: new FormControl('', [Validators.required]),
+    pet: new FormControl('', [Validators.required]),
+    start_date: new FormControl('', [Validators.required]),
+    end_date: new FormControl('', [Validators.required]),
+    cost: new FormControl('', [Validators.required]),
+    observations: new FormControl('', [Validators.required]),
+    approved: new FormControl('', [Validators.required]),
   })
 
   constructor(
     private readonly hostingService: HostingService,
-    private readonly router : Router
+    private readonly router: Router
   ) { }
 
-  ngOnInit(): void { }
+  ngOnInit(): void {
+    this.formHosting.controls['approved'].setValue(false);
+    this.formHosting.controls['owner'].setValue(1); // Pegar a partir da autenticação
 
-  makeHosting(){
-    if(this.formHosting.valid){
+    this.formHosting.controls['cost'].setValue(250); // Mudar dinamicamente
+  }
+
+  makeHosting() {
+    if (this.formHosting.valid) {
       let hosting = Object.assign({}, this.formHosting.value);
 
       this.hostingService.sendHosting(hosting).subscribe({
@@ -43,8 +49,7 @@ export class HostingComponent implements OnInit {
     }
   }
 
-
-  redirect(){
+  redirect() {
     this.router.navigate(['/home']);
   }
 }
