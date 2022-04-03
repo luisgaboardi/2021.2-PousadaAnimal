121774dcddad3d339debac0acf72840f8ea3df3f
luisgaboardi <luis.ggaboardi@gmail.com>
2022-04-03 00:39:08 -0300

    Fix

--- djangoBackend/settings.py
+++ djangoBackend/settings.py
@@ -34,9 +34,11 @@ DEBUG = env('DEBUG')
 
 ALLOWED_HOSTS = ['*']
 
-CORS_ALLOWED_ORIGINS = [
-    '*',
-]
+# CORS_ALLOWED_ORIGINS = [
+#     '*',
+# ]
+
+CORS_ORIGIN_ALLOW_ALL = True
 
 # Application definition
 
