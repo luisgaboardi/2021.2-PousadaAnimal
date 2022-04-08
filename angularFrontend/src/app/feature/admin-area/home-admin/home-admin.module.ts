3e4301c87959f9d80b50c1ece43a8cc086c3cb80
luisgaboardi <luis.ggaboardi@gmail.com>
2022-04-08 19:20:24 -0300

    FIX home-admin

---
+++ angularFrontend/src/app/feature/admin-area/home-admin/home-admin.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { Routes, RouterModule } from '@angular/router';
+import { NgxMaskModule } from 'ngx-mask';
+import { HomeAdminComponent } from './home-admin.component';
+
+
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeAdminComponent
+  }
+];
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    RouterModule.forChild(routes),
+    FormsModule,
+    ReactiveFormsModule,
+    NgxMaskModule.forRoot(),
+  ]
+})
+export class HomeAdminModule { }
